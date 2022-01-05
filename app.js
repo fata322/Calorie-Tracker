400911813140f217f9b824cca0750534b643f4e8
Marc Pacaldo <fatafatafata322@gmail.com>
2022-01-06 04:07:33 +0800

    changed some functions

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-const form = document.getElementById("form");
-form.addEventListener("submit", handleSubmit);
-
-function handleSubmit(event) {
-  event.preventDefault();
-
-  const gender = getSelectedValue("gender");
-  const age = getInputNumberValue("age");
-  const weight = getInputNumberValue("weight");
-  const height = getInputNumberValue("height");
-  const activityLevel = getSelectedValue("activity_level");
+function bmrCals() {
+  function find(id) {
+    return document.getElementById(id);
+  }
+  var age = find("age").value;
+  var height = find("height").value;
+  var weight = find("weight").value;
+  var result = 0;
+  if (find("male").checked) {
+    result = 10 * weight + 6.25 * height - 5 * age + 5;
+  } else if (find("female").checked)
+    result = 10 * weight + 6.25 * height - 5 * age - 161;
+  find("bmrCals").innerHTML = result;
 }
